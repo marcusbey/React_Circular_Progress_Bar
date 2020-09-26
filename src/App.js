4791bb675c6ab2a08d7d95b5ad4503af9b42d92e
Romain BOBOE <rboboe@gmail.com>
2020-09-26 15:37:49 -0400

    adding dependancies and structure to add.js adding dependancies and structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 import "./styles.css";
 
 export default function App() {
+  const { number, rotate, color } = useSpring({
+    from: { number: 0, rotate: 0, color: "red" },
+    number: 100,
+    rotate: 100,
+    color: "green"
+  });
   return (
-    <div className="container">
-      <div className="wrapper">
-        <svg>
-          <circle className="progress" cx="90" cy="90" r="80"></circle>
-          <circle cx="90" cy="90" r="60"></circle>
-        </svg>
-        <div className="number">
-          <h2>65$</h2>
+    <animated.div style={{}}>
+      <div className="container">
+        <div className="wrapper">
+          <svg>
+            <circle className="progress" cx="90" cy="90" r="80"></circle>
+            <circle cx="90" cy="90" r="60"></circle>
+          </svg>
+          <div className="number">
+            <h2>65$</h2>
+          </div>
         </div>
       </div>
-    </div>
+    </animated.div>
   );
 }
