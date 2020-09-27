415ffac76425f54f660bb0e474d48df63f2e505a
Romain BOBOE <rboboe@gmail.com>
2020-09-27 14:12:32 -0400

    adding function to retrieve window size

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
+import useWindowSize from "./asset/useWindowSize";
 import "./styles.css";
 
 export default function App() {
-  const { number, rotate, color } = useSpring({
-    from: { number: 0, rotate: 0, color: "red" },
-    number: 100,
-    rotate: 100,
-    color: "green"
-  });
-  return (
-    <animated.div style={{}}>
-      <div className="container">
-        <div className="wrapper">
-          <svg>
-            <circle className="progress" cx="90" cy="90" r="80"></circle>
-            <circle cx="90" cy="90" r="60"></circle>
-          </svg>
-          <div className="number">
-            <h2>65$</h2>
-          </div>
-        </div>
-      </div>
-    </animated.div>
-  );
+  // // Usage
+  // function App() {
+  //   const size = useWindowSize();
+
+  //   return (
+  //     <div>
+  //       {size.width}px / {size.height}px
+  //     </div>
+  //   );
+  // }
+
+  return <animated.div></animated.div>;
 }
